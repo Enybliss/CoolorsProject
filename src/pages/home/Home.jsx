9695921a9d4cb10ca4b2ba0aec65db186133682d
Enybliss <ulokoblessing32@gmail.com>
2024-10-28 08:14:32 +0100

    files

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import Hero from "../../components/hero/Hero"
+import Hero from "../../components/Hero/Hero"
 import SuperApps from "../../components/SuperApps/SuperApps"
 import Comp from "../../components/Companies/Comp";
 import './Home.css'
