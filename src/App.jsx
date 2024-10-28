dcf4831904333957847ef19d951b7c909dff755c
Enybliss <ulokoblessing32@gmail.com>
2024-10-28 08:02:26 +0100

    messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import Header from "./static/Header/Header";
-import Home from "./pages/home/Home"
-import Footer from "./static/footer/Footer"
+import Header from "./static/header/Header";
+import Home from "./pages/home/Home";
+import Footer from "./static/footer/Footer";
 
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+};
 
-
-const App = ()=> {
-  return(
-    <div>
-        <Header />
-        <Home />
-        <Footer />
-    </div>
-  )
-}
-
-export default App
+export default App;
